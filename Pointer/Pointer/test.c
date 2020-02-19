e278b7b08e94c79999088fef9a2df6ccc9b54a51
REDE453 <2433381382@qq.com>
2020-02-19 21:36:31 +0800

    提交代码

--- Pointer/Pointer/test.c
+++ Pointer/Pointer/test.c
@@ -57,39 +57,52 @@
 //	system("pause");
 //	return 0;
 //}
-void print_arr1(int arr[3][5], int row, int col)
+//void print_arr1(int arr[3][5], int row, int col)
+//{
+//	int i = 0, j = 0;
+//	for (i = 0; i < row; i++)
+//	{
+//		for (j = 0; j < col; j++)
+//		{
+//			printf("%d ", arr[i][j]);
+//		}
+//		printf("\n");
+//	}
+//}
+//void print_arr2(int(*arr)[5], int row, int col)
+//{
+//	int i = 0, j = 0;
+//	for (i = 0; i < row; i++)
+//	{
+//		for (j = 0; j < col; j++)
+//		{
+//			printf("%d ", arr[i][j]);
+//		}
+//		printf("\n");
+//	}
+//}
+//int main()
+//{
+//	int arr[3][5] = { 1,2,3,4,5,6,7,8,9,10 };
+//	print_arr1(arr, 3, 5);
+//	//数组名arr，表示首元素的地址
+//	//但是二维数组的首元素是二维数组的第一行
+//	//所以这里传递的arr，其实相当于第一行的地址，是一维数组的地址
+//	//可以数组指针来接收
+//	print_arr2(arr, 3, 5);
+//	system("pause");
+//	return 0;
+//}
+void test()
 {
-	int i = 0, j = 0;
-	for (i = 0; i < row; i++)
-	{
-		for (j = 0; j < col; j++)
-		{
-			printf("%d ", arr[i][j]);
-		}
-		printf("\n");
-	}
+	printf("hehe");
 }
-void print_arr2(int(*arr)[5], int row, int col)
-{
-	int i = 0, j = 0;
-	for (i = 0; i < row; i++)
-	{
-		for (j = 0; j < col; j++)
-		{
-			printf("%d ", arr[i][j]);
-		}
-		printf("\n");
-	}
-}
+//()是函数调用运算符
 int main()
 {
-	int arr[3][5] = { 1,2,3,4,5,6,7,8,9,10 };
-	print_arr1(arr, 3, 5);
-	//数组名arr，表示首元素的地址
-	//但是二维数组的首元素是二维数组的第一行
-	//所以这里传递的arr，其实相当于第一行的地址，是一维数组的地址
-	//可以数组指针来接收
-	print_arr2(arr, 3, 5);
+	test();
+	//printf("%p\n", test);
+	printf("%p\n", &test);
 	system("pause");
 	return 0;
 }
