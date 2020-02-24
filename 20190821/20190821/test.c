66499f2fb195b60bb64ea022413deabb6e29de09
REDE453 <2433381382@qq.com>
2020-02-24 23:28:39 +0800

    提交代码

---
+++ 20190821/20190821/test.c
@@ -0,0 +1,62 @@
+#include<stdio.h>
+#include<windows.h>
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	a = a ^ b;
+//	b = a ^ b;
+//	a = a ^ b;
+//	printf("%d %d", a, b);
+//	system("pause");
+//	return 0;
+//}
+//int main()
+//{
+//	int arr[10] = { 21,32,45,65,78,98,53,23,12,74 };
+//	int i = 0;
+//	int max;
+//	max = arr[0];
+//	for (i = 1; i < 10; i++)
+//	{
+//		if (max < arr[i])
+//		{
+//			max = arr[i];
+//		}
+//	}
+//	printf("%d\n", max);
+//	system("pause");
+//	return 0;
+//}
+//int main()
+//{
+//	int a, b, c;
+//	int min = 0;
+//	scanf_s("%d%d%d", &a, &b, &c);
+//	if (a < b)
+//	{
+//		min = a;
+//		a = b;
+//		b = min;
+//	}
+//	if (a < c)
+//	{
+//		min = a;
+//		a = c;
+//		c = min;
+//	}
+//	if (b < c)
+//	{
+//		min = b;
+//		b = c;
+//		c = min;
+//	}
+//	printf("%d %d %d", a, b, c);
+//	system("pause");
+//	return 0;
+//}
+int main()
+{
+	system("pause");
+	return 0;
+}
