1087c1cedf41405b576aee0437f894869f571533
REDE453 <2433381382@qq.com>
2020-02-27 20:16:07 +0800

    提交代码

--- Pointer/Pointer/test.c
+++ Pointer/Pointer/test.c
@@ -54,47 +54,49 @@
 //int main()
 //{
 //	int arr[10] = { 0 };
-//	printf("%p\n", arr);
-//	printf("%p\n", &arr);
+//	printf("arr=%p\n", arr);
+//	printf("&arr=%p\n", &arr);
+//	printf("arr+1=%p\n", arr+1);
+//	printf("&arr+1=%p\n", &arr+1);
 //	system("pause");
 //	return 0;
-//}
-//void print_arr1(int arr[3][5], int row, int col)
-//{
-//	int i = 0, j = 0;
-//	for (i = 0; i < row; i++)
-//	{
-//		for (j = 0; j < col; j++)
-//		{
-//			printf("%d ", arr[i][j]);
-//		}
-//		printf("\n");
-//	}
-//}
-//void print_arr2(int(*arr)[5], int row, int col)
-//{
-//	int i = 0, j = 0;
-//	for (i = 0; i < row; i++)
-//	{
-//		for (j = 0; j < col; j++)
-//		{
-//			printf("%d ", arr[i][j]);
-//		}
-//		printf("\n");
-//	}
-//}
-//int main()
-//{
-//	int arr[3][5] = { 1,2,3,4,5,6,7,8,9,10 };
-//	print_arr1(arr, 3, 5);
-//	//数组名arr，表示首元素的地址
-//	//但是二维数组的首元素是二维数组的第一行
-//	//所以这里传递的arr，其实相当于第一行的地址，是一维数组的地址
-//	//可以数组指针来接收
-//	print_arr2(arr, 3, 5);
-//	system("pause");
-//	return 0;
-//}
+//}    
+void print_arr1(int arr[3][5], int row, int col)
+{
+	int i = 0, j = 0;
+	for (i = 0; i < row; i++)
+	{
+		for (j = 0; j < col; j++)
+		{
+			printf("%d ", arr[i][j]);
+		}
+		printf("\n");
+	}
+}
+void print_arr2(int(*arr)[5], int row, int col)
+{
+	int i = 0, j = 0;
+	for (i = 0; i < row; i++)
+	{
+		for (j = 0; j < col; j++)
+		{
+			printf("%d ", arr[i][j]);
+		}
+		printf("\n");
+	}
+}
+int main()
+{
+	int arr[3][5] = { 1,2,3,4,5,6,7,8,9,10 };
+	print_arr1(arr, 3, 5);
+	//数组名arr，表示首元素的地址
+	//但是二维数组的首元素是二维数组的第一行
+	//所以这里传递的arr，其实相当于第一行的地址，是一维数组的地址
+	//可以数组指针来接收
+	print_arr2(arr, 3, 5);
+	system("pause");
+	return 0;
+}
 //void test()
 //{
 //	printf("hehe");
@@ -219,69 +221,69 @@ int main()
 //	ret = arr[choice - 1](num1, num2);
 //	return 0;
 //}
-void Swap(int* x, int* y)
-{
-	int tmp = *x;
-	*x = *y;
-	*y = tmp;
-}
-//Cmp是一个函数指针类型，Cmp对应到的函数应该是有两个int参数，同时返回值也是int
-typedef int(*Cmp)(int, int);
-//函数指针/回调函数通过回调函数来指定比较规则
-void BubbleSort(int arr[], int size,Cmp cmp)
-{
-	//每次找最小元素
-	//[0,bound)已排序区间
-	//(bound,size]待排序区间
-	int bound = 0;
-	//第一重循环，每次循环都找出一个最小的元素并放在靠前的位置
-	for (; bound < size; ++bound)
-	{
-		//第二重循环，具体完成找找最小的过程
-		for (int i = size - 1; i > bound; --i)
-		{
-			if (cmp(arr[i - 1] ,arr[i]))
-			{
-				//当前是升序排序，如果前一个元素大于后一个元素
-				//说明不符合当前的排序要求，就需要交换两个元素
-				Swap(&arr[i - 1], &arr[i]);
-			}
-		}
-	}
-}
+//void Swap(int* x, int* y)
+//{
+//	int tmp = *x;
+//	*x = *y;
+//	*y = tmp;
+//}
+////Cmp是一个函数指针类型，Cmp对应到的函数应该是有两个int参数，同时返回值也是int
+//typedef int(*Cmp)(int, int);
+////函数指针/回调函数通过回调函数来指定比较规则
+//void BubbleSort(int arr[], int size,Cmp cmp)
+//{
+//	//每次找最小元素
+//	//[0,bound)已排序区间
+//	//(bound,size]待排序区间
+//	int bound = 0;
+//	//第一重循环，每次循环都找出一个最小的元素并放在靠前的位置
+//	for (; bound < size; ++bound)
+//	{
+//		//第二重循环，具体完成找找最小的过程
+//		for (int i = size - 1; i > bound; --i)
+//		{
+//			if (cmp(arr[i - 1] ,arr[i]))
+//			{
+//				//当前是升序排序，如果前一个元素大于后一个元素
+//				//说明不符合当前的排序要求，就需要交换两个元素
+//				Swap(&arr[i - 1], &arr[i]);
+//			}
+//		}
+//	}
+//}
 ////////////////////////////////////////////////////////
 /////这是分割线，分割线以上的代码，可能是其他人提供的
 //////////////////////////////////////////////////////
-int Less(int x, int y)
-{
+//int Less(int x, int y)
+//{
 	//这个函数指定了升序排序的规则
 	//x是前一个元素，y是后一个元素
 	//这个函数返回一个真假值，需要交换的时候返回真不需要交换的时候返回假
-	if (x < y)
-	{
-		return 0;
-	}
-	return 1;
-}
-int Greater(int x, int y)
-{
-		//这个函数指定了降序排序的规则
+//	if (x < y)
+//	{
+//		return 0;
+//	}
+//	return 1;
+//}
+//int Greater(int x, int y)
+//{
+//		//这个函数指定了降序排序的规则
 	//x是前一个元素，y是后一个元素
 	//这个函数返回一个真假值，需要交换的时候返回真不需要交换的时候返回假
-	if (x > y)
-	{
-		return 0;
-	}
-	return 1;
-}
-int main()
-{
-	int arr1[] = { 9,5,3,7 };
-	//升序排序
-	BubbleSort(arr1, 4,Less);
-	int arr2[] = { 2,4,6,5 };
-	//降序排序
-	BubbleSort(arr2, 4, Greater);
-	system("pause");
-	return 0;
-}
+//	if (x > y) 0
+//	{
+//		return 0;
+//	}
+//	return 1;
+//}
+//int main()
+//{
+//	int arr1[] = { 9,5,3,7 };
+//	//升序排序
+//	BubbleSort(arr1, 4,Less);
+//	int arr2[] = { 2,4,6,5 };
+//	//降序排序
+//	BubbleSort(arr2, 4, Greater);
+//	system("pause");
+//	return 0;
+//}
