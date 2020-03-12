a03e4e9202784bbe2aaf7b7fef329335838109bd
REDE453 <2433381382@qq.com>
2020-03-12 21:38:54 +0800

    提交代码

---
+++ 20190818/20190818/teat.c
@@ -0,0 +1,74 @@
+#include<stdio.h>
+#include<windows.h>
+#include<string.h>
+//void func(char str[5], int time)
+//{
+//	int i = 0;
+//	int j = 0;
+//	int temp = 0;
+//	for (i = 0; i < time; i++)
+//	{
+//		for (j = 0; j < 3; j++)
+//		{
+//			temp = str[j];
+//			str[j] = str[j + 1];
+//			str[j + 1] = temp;
+//		}
+//	}
+//}
+//int main()
+//{
+//	int time =2;
+//	char str[] = "ABCD";
+//	func(str,time);
+//	printf("%s\n", str);
+//	system("pause");
+//	return 0;
+//}
+//void func(char str1[], char str2[],int time)
+//{
+//	int i = 0;
+//	int j = 0;
+//	int temp = 0;
+//	for (i = 0; i < time; i++)
+//	{
+//		for (j = 0; j < 4; j++)
+//		{
+//			temp = str1[j];
+//			str1[j] = str1[j + 1];
+//			str1[j + 1] = temp;
+//		}
+//	}
+//}
+//int main()
+//{
+//	char str1[] = "AABCD";
+//	char str2[] = "BCDAA";
+//	int time=2;
+//	func(str1, str2, time);
+//	if (strcmp(str1,str2)==0)
+//	{
+//		printf("str2是str1左旋2次得到的\n");
+//	}
+//	else
+//		printf("str2不是str1左旋2次得到的\n");
+//		system("pause");
+//		return 0;
+//}
+//void func(int arr[])
+//{
+//	int i = 0;
+//	int x = 0;
+//	for (i = 0; i < 8; i++)
+//	{
+//		x ^= arr[i];
+//	}
+//	printf("%d ", x);
+//}
+//int main()
+//{
+//	int arr[8] = { 1,1,2,2,3,3,4};
+//	func(arr);
+//	system("pause");
+//	return 0;
+//}
