cd1506a0d95e97dd3b95cd7fe0345004376a3ed8
REDE453 <2433381382@qq.com>
2020-03-17 23:30:38 +0800

    顺序表

---
+++ SeqList/SeqList/SeqList.c
@@ -0,0 +1,17 @@
+#include"SeqList.h"
+//基本增删查改
+void SeqListInit(SeqList* psl, size_t capacity);
+void SeqListDestory(SeqList* psl);
+void CheckCapacity(SeqList* psl);
+void SeqListPushBack(SeqList* psl, SLDataType x);
+void SeqListPopBack(SeqList* psl);
+void SeqListPushFront(SeqList* psl, SLDataType x);
+void SeqListPopFront(SeqList* psl);
+int SeqListFind(SeqList* psl, SLDataType x);
+void SeqListInsert(SeqList* psl, size_t pos, SLDataType x);
+void SeqListErase(SeqList* psl, size_t pos);
+void SeqListRemove(SeqList* psl, SLDataType x);
+void SeqListModify(SeqList* psl, size_t pos, SLDataType x);
+void SeqListPrint(SeqList* psl);
+// 扩展面试题实现
+void SeqListBubbleSort(SeqList* psl);
