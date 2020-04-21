1f2da9975a80ba0c4fcb2abae37f68dbe61061a5
REDE453 <2433381382@qq.com>
2020-04-21 22:55:20 +0800

    提交代码

---
+++ 20200421/20200421/test.c
@@ -0,0 +1,64 @@
+#include<stdio.h>
+#include<windows.h>
+//int main()
+//{
+//	printf("%d\n", sizeof(char));//1
+//	printf("%d\n", sizeof(short));//2
+//	printf("%d\n", sizeof(int));//4
+//	printf("%d\n", sizeof(long));//4
+//	printf("%d\n", sizeof(long long));//8
+//	printf("%d\n", sizeof(float));//4
+//	printf("%d\n", sizeof(double));//8
+//	printf("%d\n", sizeof(long double));//8
+//	system("pause");
+//	return 0;
+//}
+//int main()
+//{
+//	printf("%d\n", strlen("abcdef"));
+//	// \32被解析成一个转义字符
+//	printf("%d\n", strlen("c:\test\328\test.c"));
+//	system("pause");
+//	return 0;
+//}
+//void test()
+//{
+//	int i = 0;
+//	i++;
+//	printf("%d ", i);
+//}
+//int main()
+//{
+//	for (i = 0; i < 10; i++)
+//	{
+//		test();
+//	}
+//	system("pause");
+//	return 0;
+//}
+//代码2
+//void test()
+//{
+//	//static修饰局部变量
+//	static int i = 0;
+//	i++;
+//	printf("%d ", i);
+//}
+//int main()
+//{
+//	for (i = 0; i < 10; i++)
+//	{
+//		test();
+//	}
+//	system("pause");
+//	return 0;
+//}
+//int main()
+//{
+//	printf("%d\n", sizeof(char *));//4
+//	printf("%d\n", sizeof(short *));//4
+//	printf("%d\n", sizeof(int *));//4
+//	printf("%d\n", sizeof(double *));//4
+//	system("pause");
+//	return 0;
+//}
