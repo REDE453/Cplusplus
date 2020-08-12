2da370af79eac8444241ae2ca80ff625c55372b1
REDE453 <2433381382@qq.com>
2020-08-12 23:25:54 +0800

    提交代码

---
+++ 20200812/20200812/test.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<Windows.h>
+
+using namespace std;
+//int main()
+//{
+//	//C
+//	int* p = (int*)malloc(sizeof(int));
+//	int * p1 = (int*)malloc(sizeof(int) * 10);
+//	free(p);
+//	free(p1);
+//	//C++
+//	int * p2 = new int;
+//	int* p3 = new int(10);//申请十个空间初始化为10
+//	int* p4 = new int[10];//申请10个int类型的空间
+//	delete p3;
+//	delete[] p4;
+//	return 0;
+//}
+int main()
+{
+	return 0;
+}
